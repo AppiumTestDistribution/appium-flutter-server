950c706b69e9cc164142ddabc2ba72cf7c9c45cf
Sai Krishna <saikrishna321@yahoo.com>
2024-07-20 23:58:24 +0530

    consider elementWaitTimeout from capabilities (#23)
    * consider elementWaitTimeout from capabilities
    
    Co-authored-by: tw-manjesh <cv.manjesh@thoughtworks.com>
    Co-authored-by: rahul-kumar-sdet <rahul.kumar.sdet@gmail.com>
    Co-authored-by: akshathamkanavi <akshatha@thoughtworks.com>
    Co-authored-by: purniram <purnima.ramkumar@thoughtworks.com>
    Co-authored-by: dharsanasridharan <dharsana.sridharan@thoughtworks.com>
    
    * update release version
    
    * fixed comments
    
    ---------
    
    Co-authored-by: tw-manjesh <cv.manjesh@thoughtworks.com>
    Co-authored-by: rahul-kumar-sdet <rahul.kumar.sdet@gmail.com>
    Co-authored-by: akshathamkanavi <akshatha@thoughtworks.com>
    Co-authored-by: purniram <purnima.ramkumar@thoughtworks.com>
    Co-authored-by: dharsanasridharan <dharsana.sridharan@thoughtworks.com>

---
+++ server/lib/src/utils/flutter_settings.dart
@@ -0,0 +1,32 @@
+class FlutterSettings {
+  Map<String, dynamic> _settings = {};
+
+  FlutterSettings() {
+    _setDefaultSetting();
+  }
+
+  _setDefaultSetting() {
+    _settings = {
+      'flutterElementWaitTimeout': 5000
+    };
+  }
+
+  updateSetting(Map<String, dynamic> capabilities) {
+    // if the capability sent by user is present in the settings, then update it.
+    // Else the capability is not known to flutter server and ignore
+    capabilities.forEach((settingName, value) {
+    if (_settings[settingName] != null) {
+        _settings[settingName] = value;
+    }
+    });
+  }
+
+  dynamic getSetting(String settingName) {
+    return _settings[settingName];
+  }
+
+  // Called at the end of session
+  reset() {
+    _setDefaultSetting();
+  }
+}
