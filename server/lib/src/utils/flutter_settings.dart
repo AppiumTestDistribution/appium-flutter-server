a24cfed4d1b65b709d88e6152c332fc2b8e1b3f2
Sudharsan Selvaraj <sudharsanselvaraj.c@gmail.com>
2024-07-21 15:28:51 +0530

    Fix getText to support TextField

--- server/lib/src/utils/flutter_settings.dart
+++ server/lib/src/utils/flutter_settings.dart
@@ -1,23 +1,22 @@
 class FlutterSettings {
   Map<String, dynamic> _settings = {};
+  static const String flutterElementWaitTimeout = "flutterElementWaitTimeout";
 
   FlutterSettings() {
     _setDefaultSetting();
   }
 
   _setDefaultSetting() {
-    _settings = {
-      'flutterElementWaitTimeout': 5000
-    };
+    _settings = {flutterElementWaitTimeout: 5000};
   }
 
   updateSetting(Map<String, dynamic> capabilities) {
     // if the capability sent by user is present in the settings, then update it.
     // Else the capability is not known to flutter server and ignore
     capabilities.forEach((settingName, value) {
-    if (_settings[settingName] != null) {
+      if (_settings[settingName] != null) {
         _settings[settingName] = value;
-    }
+      }
     });
   }
 
